13e537b4552906b7b46b6661080b4b62da28437d
robinucar <mehmet.ucar@imediapro.co.uk>
2021-02-10 16:05:07 +0000

    appiservices, fixtures and players data from api

---
+++ src/helpers/apiServices.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+const token =
+  'eFExWCgsmCX066dKSsJVmGu2HUYATekkGFcxbgnIfjPTtQ67JZrWjDnmwS3dn93p';
+//getting fixtures data
+const getFixture = async (fixtureId) => {
+  return axios
+    .get(
+      'http://dev.webuildbots.ai:9123/fixtures?fixtureId=' +
+        fixtureId +
+        '&token=' +
+        token
+    )
+    .then(async (res) => res.data)
+    .catch((err) => console.error(err));
+};
+
+//getting player data
+const getPlayer = async (playerId) => {
+  return axios
+    .get(
+      'http://dev.webuildbots.ai:9123/players?playerId=' +
+        playerId +
+        '&token=' +
+        token
+    )
+    .then((res) => res.data)
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+export { getFixture, getPlayer };
