b1fe8a09747a87cfa826d9af7f63c2547c61b33b
robinucar <mehmet.ucar@imediapro.co.uk>
2021-02-10 17:46:06 +0000

    getting data to ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,102 @@
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { ADD_FIXTURE } from './redux/actions/types';
+import { getFixture, getPlayer } from './helpers/apiServices';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>React project</h1>
-    </div>
-  );
-}
+const App = () => {
+  //useDispatch
+  const dispatch = useDispatch();
+  //getting state
+  const fixture = useSelector((state) => state.fixture);
+  //loading
+  const [load, setLoad] = useState(false);
+
+  useEffect(() => {
+    getData('1234567');
+  }, []);
+
+  const getData = async (fixtureId) => {
+    getFixture(fixtureId)
+      .then(async (res) => {
+        //home Team
+        const team1 = res.teams[0].players.map(async (d, i) => {
+          //getting hometeam players detail with player id
+          const player = await getPlayer(d.playerId);
+          return {
+            playerId: d.playerId,
+            position: d.position,
+            playerName: player.name,
+            playerNo: player.played,
+            playerGoals: player.goals,
+            playerRedCards: player.redCards,
+            playerYellowCards: player.yellowCards,
+          };
+        });
+        //second Team
+        const team2 = res.teams[1].players.map(async (d, i) => {
+          //getting second team players details with player id
+          const player = await getPlayer(d.playerId);
+          return {
+            playerId: d.playerId,
+            position: d.position,
+            playerName: player.name,
+            playerNo: player.played,
+            playerGoals: player.goals,
+            playerRedCards: player.redCards,
+            playerYellowCards: player.yellowCards,
+          };
+        });
+        //teams informations
+        const resultTeam1 = Promise.all(team1);
+        const resultTeam2 = Promise.all(team2);
+
+        res.teams[0].players = await resultTeam1;
+        res.teams[1].players = await resultTeam2;
+
+        //dispatch data to store
+        dispatch({ type: ADD_FIXTURE, payload: res });
+
+        setLoad(true);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  if (load) {
+    return (
+      <div className='app'>
+        <div>
+          {fixture.teams[0].name} - {fixture.teams[1].name}
+        </div>
+        <div>{fixture.location}</div>
+        <div>{fixture.date}</div>
+        <div>
+          <div>
+            {fixture.teams[0].players.map((d, i) => {
+              return (
+                <div key={d.playerId}>
+                  <b>{d.playerNo}</b> {d.playerName} - {d.position}
+                </div>
+              );
+            })}
+          </div>
+          <hr />
+          <div>
+            {fixture.teams[1].players.map((d, i) => {
+              return (
+                <div key={d.playerId}>
+                  <b>{d.playerNo}</b> {d.playerName} - {d.position}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return <div className='app'>Loading...</div>;
+  }
+};
 
 export default App;
